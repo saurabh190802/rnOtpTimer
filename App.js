6169b3f1fa8e67370909d3bd0c936521a56ece9a
saurabh satapathy <saurabhsatapathy0@gmail.com>
2022-06-30 13:32:32 +0530

    final

--- App.js
+++ App.js
@@ -4,8 +4,24 @@ import RnOtpTimer from './components/RnOtpTimer';
 
 const App = () => {
   return (
-    <View style={{flex:1,justifyContent:'center',flexDirection:'column',alignSelf:'center'}}>
-      <RnOtpTimer />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignSelf: 'center',
+      }}>
+      <RnOtpTimer
+        minute={1}
+        second={30}
+        fontsize={20}
+        fontfamily={'Poppins-Regular'}
+        textcolor={'blue'}
+        labelfontsize={20}
+        labelfontfamily={'Poppins-Regular'}
+        labelcolor={'blue'}
+        labeltext="want otp again"
+      />
     </View>
   );
 };
